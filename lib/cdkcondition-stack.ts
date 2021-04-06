85a3de769b2e9f8af4eb85caa51e60dff1e3f09c
Ken Suzuki <suzukiken@gmail.com>
2021-04-06 10:17:55 +0900

    done

--- lib/cdkcondition-stack.ts
+++ lib/cdkcondition-stack.ts
@@ -4,6 +4,19 @@ export class CdkconditionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // contextパラメーターはcdk.jsonなどに書くことができる
+    // ここで指定しているのは便宜的なもの
+    this.node.setContext('stage', 'dev')
+
+    const stage = this.node.tryGetContext('stage');
+
+    const condition = new cdk.CfnCondition(this, "condition", {
+      expression: cdk.Fn.conditionEquals(stage, "dev")
+    })
+
+    const conditval = cdk.Fn.conditionIf(
+    	condition.logicalId, "stage is dev", "stage is not dev")
+
+    new cdk.CfnOutput(this, 'output', {value: conditval.toString()})
   }
 }
